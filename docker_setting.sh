40151f653f03793300423771120ef673b460deef
chosung-dev <chosung.dev@gmail.com>
2019-05-14 15:21:17 +0900

    bash명령어 추가

--- docker_setting.sh
+++ docker_setting.sh
@@ -1,9 +1,11 @@
 #docker 관련
+touch ~/.bash_aliases
+echo "
 alias dockermi='docker rmi $(docker images -aq)'
 alias dockerm='docker rm -f $(docker ps -aq)'
 alias dockermall='dockerm; dockermi; docker network prune;'
 alias dockerl='echo -e ${GREEN}DOCKER IMAGES${NC};
                 docker images; 
                 echo -e ${GREEN}DOCKER PS${NC};
-                docker ps'
-
+                docker ps'">.bash_aliases
+source .bash_aliases
