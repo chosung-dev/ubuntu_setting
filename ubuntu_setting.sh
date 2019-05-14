2594410c2a581bdeef7525975e9d61d281a9ff92
chosung-dev <chosung.dev@gmail.com>
2019-05-14 14:10:08 +0900

    화면보호기 끄기

--- ubuntu_setting.sh
+++ ubuntu_setting.sh
@@ -6,9 +6,14 @@ NC='\033[0m'
 echo "chosung linux setting start"
 sudo apt-get update
 
-
+#Systme Setting
 #사이드바 하단으로
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+
+#화면보호기 끄기
+xset s off
+xset -dpms
+
 
 #aliase setting
 touch ~/.bash_aliases
@@ -46,4 +51,4 @@ echo Y|sudo snap install vlc
 echo Y|sudo apt-get install libxss1 libgconf2-4 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-
+rm google-chrome-*
