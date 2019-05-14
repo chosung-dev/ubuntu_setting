a26be02fd6fb8b11a35bc05729be9173ae65040f
chosung-dev <chosung.dev@gmail.com>
2019-05-14 16:05:11 +0900

    bash 오류 수정

--- docker_setting.sh
+++ docker_setting.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 #docker 관련
-touch ~/.bash_aliases
-echo "
-alias dockermi='docker rmi $(docker images -aq)'
-alias dockerm='docker rm -f $(docker ps -aq)'
+echo "alias dockermi='docker rmi \$(docker images -aq)'
+alias dockerm='docker rm -f \$(docker ps -aq)'
 alias dockermall='dockerm; dockermi; docker network prune;'
-alias dockerl='echo -e ${GREEN}DOCKER IMAGES${NC};
+alias dockerl='echo -e \${GREEN}DOCKER IMAGES\${NC};
                 docker images; 
-                echo -e ${GREEN}DOCKER PS${NC};
-                docker ps'">.bash_aliases
-source .bash_aliases
+                echo -e \${GREEN}DOCKER PS\${NC};
+                docker ps'">>~/.bash_aliases
+
+source ~/.bash_aliases
--- ubuntu_setting.sh
+++ ubuntu_setting.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-NC='\033[0m'
-
 echo "chosung linux setting start"
 sudo apt-get update
 
@@ -14,11 +11,12 @@ gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 xset s off
 xset -dpms
 
-
 #aliase setting
 touch ~/.bash_aliases
-echo "alias l='ls -l'">.bash_aliases
-source .bash_aliases
+echo "#!/bin/bash">~/.bash_aliases
+echo "GREEN='\033[0;32m'">>~/.bash_aliases
+echo "NC='\033[0m'">>~/.bash_aliases
+echo "alias l='ls -l'">>~/.bash_aliases
 
 #VIM
 echo Y|sudo apt-get install vim
@@ -42,7 +40,7 @@ set title
 set showmatch  
 set nowrap
 set wmnu 
-syntax on">.vimrc
+syntax on">~/.vimrc
 
 # VLC 동영상 뷰어
 echo Y|sudo snap install vlc
@@ -52,3 +50,5 @@ echo Y|sudo apt-get install libxss1 libgconf2-4 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-*
+
+source ~/.bash_aliases
