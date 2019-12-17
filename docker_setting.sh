74f00829d4272f68caa75964bbb2a8fd77010f7a
chosung-dev <49059774+chosung-dev@users.noreply.github.com>
2019-12-17 16:13:45 +0900

    볼륨 삭제 추가

--- docker_setting.sh
+++ docker_setting.sh
@@ -2,7 +2,7 @@
 #docker 관련
 echo "alias dockermi='docker rmi -f \$(docker images -aq)'
 alias dockerm='docker rm -f \$(docker ps -aq)'
-alias dockermall='dockerm; dockermi; docker network prune;'
+alias dockermall='dockerm; dockermi; docker network prune; docker system  prune;'
 alias dockerl='echo -e \${GREEN}DOCKER IMAGES\${NC};
                 docker images; 
                 echo -e \${GREEN}DOCKER PS\${NC};
