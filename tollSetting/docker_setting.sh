0c1e5427e2208f6b12833d8e2c29a4b7078dc5df
chosung <noah.cho@themedium.io>
2020-07-15 17:29:29 +0900

    change dir

---
+++ tollSetting/docker_setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#docker 관련
+echo "alias dockermi='docker rmi -f \$(docker images -aq)'
+alias dockerm='docker rm -f \$(docker ps -aq)'
+alias dockermall='dockerm; dockermi; docker network prune; docker system  prune;'
+alias dockerl='echo -e \${GREEN}DOCKER IMAGES\${NC};
+                docker images; 
+                echo -e \${GREEN}DOCKER PS\${NC};
+                docker ps'">>~/.bash_aliases
+
+source ~/.bash_aliases
